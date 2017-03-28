8f6e150696bb971ec781838c1487d06bde1f68d5
ale <ale@localhost>
2017-03-28 20:43:20 +0200

    current modification

--- main.m
+++ main.m
@@ -9,9 +9,9 @@ delta_y = width / rows; %width of coil/rows
 
 r = 0.125/2; %inner radius of the coil
 d =1*r; %distance between the two coils
-I = 1.85; %current that flows through the coil
+I = 1.8; %current that flows through the coil
 
-y_vec = -0.02:(d/100):d+0.02;
+y_vec = -0.06:(d/100):d+0.06;
 H_tot = [];
 out = zeros(layers, rows);
 for y = y_vec
@@ -19,17 +19,17 @@ for y = y_vec
         for m = 1:rows
             %out(n,m) = H(r + delta_r*n, y, I) + H(r + delta_r*n, d - y, I);
             %out(n,m) = H(r + delta_r*n, y - m*delta_y, I) + H(r + delta_r*n, d - y + m*delta_y, I);
-            out(n,m) = H(r , y , I) + H(r , d - y, I);
+            out(n,m) = H(r , y , current(y)) + H(r , d - y, current(y));
         end
     end
 
 H_tot(end + 1) = sum(sum(out))*4*pi*10^-7;
 
 end
-close all
-figure
-hold on
-line([0 0], [0 max(H_tot)])
-line([d d], [0 max(H_tot)])
-plot(y_vec,H_tot)
-axis tight
+% close all
+% figure
+% hold on
+% line([0 0], [0 max(H_tot)])
+% line([d d], [0 max(H_tot)])
+% plot(y_vec,H_tot)
+% axis tight
